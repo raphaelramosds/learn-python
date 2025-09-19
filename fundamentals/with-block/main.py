9764c90a254fda2cb57385bce725be1985afcc76
Raphael Ramos <raphael.opensource@gmail.com>
2025-09-18 21:12:26 -0300

    feat: add with-block code sample

---
+++ fundamentals/with-block/main.py
@@ -0,0 +1,20 @@
+# O bloco `with` é utilizado para controlar contextos no seu programa com o "context manager protocol"
+
+
+class Foo:
+    # NOTE: classes que serao utilizadas como contexto possuem os metodos magicos __exit__ e __enter__
+    def __exit__(self, *args):
+        print("Saindo do bloco with")
+        print(args)
+
+    def __enter__(self):
+        print("Entrando no bloco with")
+
+
+x = 10
+foo = Foo()
+with foo:
+    print("Dentro do bloco")
+    x = 2
+
+print(x)  # saida: x = ?
